3989d400bc93cdb4048b482b1ae648c2b91893f5
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-04-28 11:46:17 -0700

    2x2 matrix

---
+++ c programs/accept and dist 2x2 matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int m1[2][2],i,j;
+	printf("enter 2*2 matrix");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("m1[ %d ][%d]=",i,j);
+			scanf("%d",&m1[i][j]);
+			
+		}
+	}
+	//dis
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("m1[%d][%d]= %d",i,j,m1 [i] [j]);
+		}
+	}
+	
+}
