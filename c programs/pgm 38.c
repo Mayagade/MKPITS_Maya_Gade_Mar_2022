838ee14ba4fdb9ee851e65ea4bcbe807db5684a8
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-04-28 11:00:38 -0700

    c program to print a number, it's square and cube in a line

---
+++ c programs/pgm 38.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+	int a, i, j = 1, x = 0;
+	printf("enter number of lines: ");
+	scanf("%d", &a);
+	for(i = 1; i <= a; i++) 
+	{
+		while(x < 3) {
+			printf("%d ", j++);
+			x++;
+		}
+		x = 0;
+		printf("\n");
+	}
+	
+	return 0;
+}
