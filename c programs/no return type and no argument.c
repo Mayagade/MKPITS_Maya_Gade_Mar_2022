8d862ce15a0530eb152f135c3bddba5f1ebbad7c
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-05-04 11:46:30 -0700

    no return type and no argument

---
+++ c programs/no return type and no argument.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void fun();
+int main()
+{
+	fun();
+}
+// no returntype and no argument
+void fun()
+{
+	int x1, x2;
+	printf("enter two number\n");
+	scanf("%d%d",&x1,&x2);
+	if(x1<x2)
+	{
+		printf("%d is smaller",x1);
+		
+	}
+	if(x2<x1)
+	{
+		printf("%d id smaller",x2);
+	}
+	if(x1==x2)
+	{
+		printf("both are equal");
+	}
+	
+}
+
