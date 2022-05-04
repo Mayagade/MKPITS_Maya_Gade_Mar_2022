22b31ed4bf7ca94fff7d3d12ee214d6b0eb4945b
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-05-04 11:43:04 -0700

    Add files via upload

---
+++ c programs/addition pgm of no argument and no returntype.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	add();
+}
+void add()
+{
+	int num1,num2,res;
+	printf("enter any num");
+	scanf("%d",&num1);
+	printf("enter any number");
+	scanf("%d",&num2);
+	res=num1+num2;
+	printf("res=%d",res);
+	
+}
