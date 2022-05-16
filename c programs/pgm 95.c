9bc851bcfc71ba3e8390dd84b87d25a930a0878c
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-05-16 01:43:14 -0700

    Add files via upload

---
+++ c programs/pgm 95.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void)
+{
+    printf("%d",test(3));
+    printf("\n%d",test(7));
+    printf("\n%d",test(8));
+    printf("\n%d",test(21));
+    }   
+    
+    int test(int n)
+         {
+            return n % 10 <= 2 || n % 10 >= 8;
+         }
