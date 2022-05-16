d6356f611cad42c0b0cb3646cdd247393eb2ebe4
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-05-16 01:15:40 -0700

    Add files via upload

---
+++ c programs/pgm 83.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void)
+   {
+    printf("%d",num(20, 84));
+    printf("\n%d",num(14, 50));
+    printf("\n%d",num(11, 45));
+    printf("\n%d",num(25, 40));
+    }   
+   int num(int x, int y)
+        {
+             return (x <= 20 || y >= 50) ||
+			  (x <= 20 || y >= 50);
+
+        }
+
