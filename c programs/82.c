c51370ba7d2d7ba1750f31ccc4dc83ace5f776aa
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-05-16 00:34:36 -0700

    to check weather thee given integer values are in the range 20..50

---
+++ c programs/82.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("%d",test(11, 20, 12));
+    printf("%d",test(30, 30, 17));
+    printf("%d",test(25, 35, 50));
+    printf("%d",test(15, 12, 8));
+    }   
+   int test(int x, int y, int z)
+        {
+            return (x >= 20 && x <= 50) || (y >= 20 && y <= 50) || (z >= 20 && z <= 50);
+        }
+
