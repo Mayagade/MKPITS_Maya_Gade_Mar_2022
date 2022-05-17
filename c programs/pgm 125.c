8f9928f3a9d53edfbcb62da8026365ebcf1387ec
Mayagade <101824800+Mayagade@users.noreply.github.com>
2022-05-17 11:10:15 -0700

    if a given aray contais 3 or a 5

---
+++ c programs/pgm 125.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[3], i, f=0;
+    printf("Enter value: ");
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<3;i++)
+    {
+        if(a[i]==3 || a[i]==5)
+        {
+            printf("True");
+            break;
+        }
+        else
+        {
+            printf("False");
+            break;
+        }
+    }
+}
+
+
